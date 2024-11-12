04d3916e70da75ed64bb3d6fd30e87c6d5023c8a
vorkutavorkutlag <royzurakipiani@gmail.com>
2024-11-13 00:43:55 +0200

    started work on parsing module and encoder

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,20 @@ use std::{process};
 
 use bencode::Bencode;
 mod bencode;
+mod torrent_reader;
 
 fn main() {
-    let filename = "sonic.torrent";
-    let torrent_hashmap = match bencode::read_torrent_file(filename) {
-        Ok(bytes) => {
-            let mut index = 0;
-            match bencode::decode_bencode(&bytes, &mut index) {
-                Ok(decoded) => decoded,
-                Err(e) => {eprintln!("Error decoding bencode: {}", e); process::exit(1)},
-            }
-        }
-        Err(e) => {eprintln!("Error reading file: {}", e); process::exit(1)},
-    };
+    let filename: &str = "sonic.torrent";
     
-    match torrent_hashmap {
-        Bencode::Dictionary(ref dict) => {
-            for (key, value) in dict.iter() {
-                println!("Key: {}, Value: {:?}", key, value);
-            }
-        }
-        _ => {
-            eprintln!("Decoded data is not a dictionary");
-            process::exit(1);
-        }
-    }
+    // match torrent_hashmap {
+    //     Bencode::Dictionary(ref dict) => {
+    //         for (key, value) in dict.iter() {
+    //             println!("Key: {}, Value: {:?}", key, value);
+    //         }
+    //     }
+    //     _ => {
+    //         eprintln!("Decoded data is not a dictionary");
+    //         process::exit(1);
+    //     }
+    // }
 }
--- src/torrent_reader.rs
+++ src/torrent_reader.rs
@@ -1,10 +1,27 @@
-use bencode::Bencode;
-mod bencode;
+use crate::bencode;
+use std::{collections::HashMap, process};
 
-fn prase_torrent(torrent_hashmap: &HashMap<String, Bencode>) {
+const SHA1_LEN: usize = 20;
+
+pub fn parse_torrent(filename: &str) {
     // hashed info == sha1 function on bencoded info key
     // announce list == announce key and announce-list key
     // piece_length == piece length key of the info key
     // size == length key of the info key or sum of all length keys in the files key in the info key
     // num_pieces == ceiiling of size / piece_length
+    let info_hash: Vec<u8> = vec![0; SHA1_LEN];
+    
+    let torrent_hashmap = match bencode::read_torrent_file(filename) {
+        Ok(bytes) => {
+
+
+            let mut index = 0;
+            match bencode::decode_bencode(&bytes, &mut index) {
+                Ok(decoded) => decoded,
+                Err(e) => {eprintln!("Error decoding bencode: {}", e); process::exit(1)},
+            }
+        }
+        Err(e) => {eprintln!("Error reading file: {}", e); process::exit(1)},
+    };
+    
 }
