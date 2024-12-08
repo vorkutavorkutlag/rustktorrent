d3c4d2f29fefb31b36e3599b557d3199250a2e39
vorkutavorkutlag <royzurakipiani@gmail.com>
2024-12-08 04:24:41 +0400

    Fixed compile issues in peers handshake. not tested yet

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use uuid::Uuid;
 mod bencode;
 mod torrent_reader;
 mod tracker;
+mod peers;
 
 #[tokio::main]
 async fn main() {
--- src/peers.rs
+++ src/peers.rs
@@ -1,41 +1,38 @@
-use std::{net::{TcpStream}};
+use std::{net::{TcpStream, SocketAddrV4, Ipv4Addr}};
 
 async fn do_handshake(t_peer: SocketAddrV4, infohash: &[u8], peer_id: &str) -> Result<TcpStream, String> {
-  let mut socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?;
+  let mut socket = TcpStream::bind((Ipv4Addr::UNSPECIFIED, 0))?;
   socket.connect(t_peer)?;
   
-  let protocol: [u8] = "Bittorrent Protocol".to_be_bytes();
-  let p_len: u8 = protocol.len();
-  let reserved: [u8] = [0u8; 8];
-  let pid_bytes: [u8] = peer_id.to_bytes();
+  let protocol: &[u8] = "Bittorrent Protocol".as_bytes();
+  let p_len: usize = protocol.len();
+  let reserved = [0u8; 8];
+  let pid_bytes: &[u8] = peer_id.as_bytes();
   
   let mut packet: Vec<u8> = vec![];
 
-  packet.extend(p_len);
+  packet.extend(&p_len.to_be_bytes());
   packet.extend(protocol);
   packet.extend(&reserved);
-  packet.extend(&info_hash);
-  packet.extend(&pid_bytes);
+  packet.extend(infohash);
+  packet.extend(pid_bytes);
 
   socket.send(&packet)?;
 
   let mut response_data: Vec<u8> = vec![0u8; 68]; 
   let (size, _) = socket.recv_from(&mut response_data)?;
 
-  let valid_response: bool = True;
+  let valid_response: bool = true;
   
   valid_response = valid_response && size == 68;
-  valid_response = valid_response && response_data[0..2] == p_len;
-  valid_response = valid_response && response_data[2..p_len] == protocol;
+  valid_response = valid_response && response_data[0..2] == p_len.to_be_bytes();
+  valid_response = valid_response && response_data[2..p_len] == *protocol;
   valid_response = valid_response && response_data[p_len..p_len+8] == reserved;
-  valid_response = valid_response && response_data[p_len+8..p_len+28] == info_hash;
+  valid_response = valid_response && response_data[p_len+8..p_len+28] == *infohash;
   
   if valid_response {
     return Ok(socket);
   }
 
-  return Err("Invalid response");
-
-
-
+  return Err("Invalid response".to_string());
 }
