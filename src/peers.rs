a8a63198f311e749626d389fa69cc1ac3ad666fa
vorkutavorkutlag <royzurakipiani@gmail.com>
2024-12-08 18:06:20 +0400

    Fixed further compile errors in peers. WIP

--- src/peers.rs
+++ src/peers.rs
@@ -1,8 +1,7 @@
-use std::{net::{TcpStream, SocketAddrV4, Ipv4Addr}};
+use std::{net::{TcpStream, SocketAddrV4}, io::prelude::*};
 
 async fn do_handshake(t_peer: SocketAddrV4, infohash: &[u8], peer_id: &str) -> Result<TcpStream, String> {
-  let mut socket = TcpStream::bind((Ipv4Addr::UNSPECIFIED, 0))?;
-  socket.connect(t_peer)?;
+  let mut socket = TcpStream::connect(t_peer).map_err(|e| e.to_string())?;
   
   let protocol: &[u8] = "Bittorrent Protocol".as_bytes();
   let p_len: usize = protocol.len();
@@ -17,14 +16,16 @@ async fn do_handshake(t_peer: SocketAddrV4, infohash: &[u8], peer_id: &str) -> R
   packet.extend(infohash);
   packet.extend(pid_bytes);
 
-  socket.send(&packet)?;
+  socket.write_all(&packet).map_err(|e| e.to_string())?;
 
   let mut response_data: Vec<u8> = vec![0u8; 68]; 
-  let (size, _) = socket.recv_from(&mut response_data)?;
+  let size = socket.read(&mut response_data).map_err(|e| e.to_string())?;
 
-  let valid_response: bool = true;
-  
-  valid_response = valid_response && size == 68;
+  if size != 68 {
+    return Err("Invalid response".to_string());
+  }
+
+  let mut valid_response: bool = true;
   valid_response = valid_response && response_data[0..2] == p_len.to_be_bytes();
   valid_response = valid_response && response_data[2..p_len] == *protocol;
   valid_response = valid_response && response_data[p_len..p_len+8] == reserved;
